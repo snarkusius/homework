2a76ce104b86d24101f3167e01cd695942678418
snarkusius <marcus@lengquist.se>
2020-11-30 10:51:53 +0100

    48

--- 48/1/script.js
+++ 48/1/script.js
@@ -1,3 +1,6 @@
 // Deklarera en variabel name och ge den värdet av ditt namn
-
+const name = "marcus"
 // Skriv en for-loop som loggar ut "Hejsan <ditt namn här>!!" 500 gånger
+for (let i = 0; i < 50; i++) {
+    console.log(`hejsan ${name}`)   
+}
--- 48/2/script.js
+++ 48/2/script.js
@@ -1,7 +1,12 @@
 // Skriv en while loop
 // Som varje iteration (repetition) slumpar ett heltal mellan 0 och 10
 // Om talet blir 6, avsluta loopen
+let random 
 
+while (random != 6) {
+    random = Math.round(math.random()*10)
+    console.log(random)
+}
 
 // Exempel på output:
 // 4
